29a6f9942078cda0d1bbd3ac2d2ffe6112c0c416
Dustin Mac <sonnyduluxwatershield@gmail.com>
2021-04-08 20:07:28 -0500

    Update class user to use chaining method

--- OOP/user.py
+++ OOP/user.py
@@ -5,40 +5,31 @@ class User:
         
     def make_deposit(self,amount):
         self.amount+=amount
+        return self
     
     def make_withdrawal(self, amount):
         self.amount-=amount
+        return self
     
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: {self.amount}")
+        return self
         
     def transfer_money(self,other_user,amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
 
 
 Liam = User("Liam",100)
 Neil = User("Neil",100)
 Harris = User("Harris",100)
 
-Liam.make_deposit(30)
-Liam.make_deposit(20)
-Liam.make_deposit(10)
-Liam.make_withdrawal(40)
-Liam.display_user_balance()
+Liam.make_deposit(30).make_deposit(20).make_deposit(10).display_user_balance()
 
-Neil.make_deposit(50)
-Neil.make_deposit(60)
-Neil.make_withdrawal(100)
-Neil.make_withdrawal(24)
-Neil.display_user_balance()
+Neil.make_deposit(50).make_deposit(60).make_withdrawal(100).make_withdrawal(24).display_user_balance()
 
-Harris.make_deposit(540)
-Harris.make_withdrawal(26)
-Harris.make_withdrawal(87)
-Harris.make_withdrawal(32)
-Harris.display_user_balance()
+Harris.make_deposit(540).make_withdrawal(26).make_withdrawal(87).make_withdrawal(32).display_user_balance()
 
-Liam.transfer_money(Harris,30)
-Liam.display_user_balance()
+Liam.transfer_money(Harris,30).display_user_balance()
 Harris.display_user_balance()
