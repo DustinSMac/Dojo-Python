2e0f4e92abc89181bdaa41345121e9858f5719f6
Dustin Mac <sonnyduluxwatershield@gmail.com>
2021-04-09 12:23:04 -0500

    Clean up the OOP folder, put class into separate files and do assignment on separate file

---
+++ OOP/OOP.py
@@ -0,0 +1,23 @@
+from user import User
+
+
+#First part for User assignment
+Liam = User("Liam",100)
+Neil = User("Neil",100)
+Harris = User("Harris",100)
+
+Liam.make_deposit(30).make_deposit(20).make_deposit(10).display_user_balance()
+
+Neil.make_deposit(50).make_deposit(60).make_withdrawal(100).make_withdrawal(24).display_user_balance()
+
+Harris.make_deposit(540).make_withdrawal(26).make_withdrawal(87).make_withdrawal(32).display_user_balance()
+
+Liam.transfer_money(Harris,30).display_user_balance()
+Harris.display_user_balance()
+
+from BankAccount import BankAccount
+#2nd part for BankAccount assignment
+AccountA=BankAccount()
+AccountA.deposit(50).deposit(50).deposit(60).withdraw(32).yield_interest().display_account_info()
+AccountB=BankAccount()
+AccountB.deposit(24).deposit(542).withdraw(12).withdraw(32).withdraw(53).withdraw(40).yield_interest().display_account_info()
--- OOP/user.py
+++ OOP/user.py
@@ -19,17 +19,3 @@ class User:
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
         return self
-
-
-Liam = User("Liam",100)
-Neil = User("Neil",100)
-Harris = User("Harris",100)
-
-Liam.make_deposit(30).make_deposit(20).make_deposit(10).display_user_balance()
-
-Neil.make_deposit(50).make_deposit(60).make_withdrawal(100).make_withdrawal(24).display_user_balance()
-
-Harris.make_deposit(540).make_withdrawal(26).make_withdrawal(87).make_withdrawal(32).display_user_balance()
-
-Liam.transfer_money(Harris,30).display_user_balance()
-Harris.display_user_balance()
